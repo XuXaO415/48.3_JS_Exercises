a26577bf2706f9a6b78afe9561943b3f3cd020af
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-09-25 18:50:37 -0700

    Completed guessing game closure exercise

--- Solving-tricky-parts/guessing-game.js
+++ Solving-tricky-parts/guessing-game.js
@@ -1,5 +1,27 @@
 function guessingGame() {
-
+    let answer = Math.floor(Math.random() * 100);
+    let guesses = 0;
+    let completed = false;
+    return function guess(num) {
+        if (completed) return "The game is over, you already won!";
+        guesses++;
+        if (num === answer) {
+            completed = true;
+            return `You win! You found ${answer} in ${guesses} guesses.`;
+        } else if (num < answer) {
+            return `${num} is too low!`;
+        } else {
+            return `${num} is too high!`;
+        }
+    };
 }
 
-module.exports = { guessingGame };
+// let game = guessingGame();
+// console.log(game(50)); // "50 is too low!"
+// console.log(game(90)); // "90 is too high!"
+// console.log(game(70)); // "You win! You found 70 in 3 guesses."
+// console.log(game(70)); // "The game is over, you already won!"
+
+module.exports = {
+    guessingGame
+};
